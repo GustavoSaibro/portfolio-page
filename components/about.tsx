5abac9be836c7e7c340e7ddab7ef100000333d27
Gustavo Saibro <gustavosaibro@gmail.com>
2024-01-14 19:55:53 -0300

    first commit

---
+++ components/about.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        My first contact with programming was still in highschool<i>(2011)</i>,
+        when I decided to create a program to calculate quadratic formula to confirm my answer, using{" "}
+        <strong>
+          <em>Pascal</em>{" "}
+        </strong>
+        . Also in this same year I was curious to know how the internet works,
+        then I started to read about internet infrastructure. After those
+        events,started my passion about computer science.
+      </p>
+
+      <p className="mb-3">
+        While studying <span className="font-medium">Physics</span>, I decided
+        to pursue my passion for programming. So then I changed my course to{" "}
+        <span className="font-medium">Information Systems</span> I started
+        looking for internal jobs halfway through the course. I got my first
+        intern in <span className="font-medium">data engineering</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem.
+      </p>
+
+      <p className="mb-3">
+        My core stack is{" "}
+        <span className="font-medium">
+          Python, SQL<em> (PostgreSQL,Oracle,MySQL,Redshift)</em>, Apache
+          Airflow, AWS, Terraform, Docker, Kubernetes and Linux.
+        </span>{" "}
+        But I also know web development, such{" "}
+        <span className="font-medium">
+          JavaScript, NestJs, Django&DjangoRest,React
+        </span>
+        . I am also familiar with TypeScript, Prisma, NextJs. I am always
+        looking to learn new technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a{" "}
+        <span className="underline">data engineer</span>.
+      </p>
+      <p className="mb-3">
+        I'm{" "}
+        <span className="font-medium">
+          <strong>
+            <em>A.I</em>
+          </strong>
+        </span>{" "}
+        enthusiast, I love study about, since the beggining of my graduation. I
+        started to learn about <span className="font-medium">Machine Learning</span>, algorithms like 
+        {" "}<span><em>Linear Regression, Logistic Regression, Support Vector Machine</em></span> and also 
+        {" "}<span className="font-medium">Deep Learning</span>, tools such 
+        {" "}<span className="font-medium"><em>TensorFlow,PyTorch,Keras,Anaconda</em></span>.
+        My undergraduate thesis was about create an <em>ML</em> model to predict the grades from students of literature classes. For this I used Natural Language Processing techniques, to transform those texts into <strong><em>vectors</em></strong> to use them as input for <em>ML</em> model and predict grades 
+      </p>
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy 
+        reading a book,{" "} watch a science video or a movie and listen music. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">history and sociology</span>.
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,154 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { LuGraduationCap } from "react-icons/lu";
+import supermarket from "@/public/supermarket.png"
+import deposit from "@/public/deposit.png"
+import astar from "@/public/astar.png"
+import full_page from "@/public/full_page_app.png"
+import tcc from "@/public/TCC.png"
+
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Start of my college",
+    location: "Florianópolis, SC",
+    description:
+      "Started the undergraduate degree in Information Systems, where learned the fundamentals of computer science, bussiness, software engineering, project management.",
+    icon: React.createElement(LuGraduationCap),
+    date: "Aug 2017 - Dec 2022",
+  },
+  {
+    title: "Data Engineer intern",
+    location: "Florianópolis, SC",
+    description:
+      "Internship to data engineering to learn the basics of maintaining databases, general concepts of database governance, managing users and tables, running required queries to get data for reports.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Jul 2019 - Dec 2019",
+  },
+  {
+    title: "Data Scientist intern",
+    location: "Florianópolis, SC",
+    description:
+      "Internship of data science to learn the basics concepts about data cleaning, data visualization, machine learning concepts, models,data dictionaries. The main goal was to organize the data in order to extract business insights, and to give an direction to decisions based in data.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Jun 2019 - Dec 2019",
+  },
+  {
+    title: "Quality Assurance",
+    location: "Florianópolis, SC",
+    description:
+      "Quality assurance of the software PEC, using techniques like black box testing, white box testing, regression testing, data validation, charge testing, security testing, graphQL testing. This software is used in almost every brazilians basic health facilities. Team lead of a new project, leading the QA team.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Apr 2021 - Mar 2022",
+  },
+  {
+    title: "Data Engineer",
+    location: "Florianópolis, SC",
+    description:
+      "Development and design a application to consumes external data from APIs to populate a database, merging multiple data sources, this application uses technologies like NodeJs, NestJs, GraphQL, Axios, RabbitMQ. Also worked in a legacy project, maintaining and improving the legacy software, to add new features and improve performance of the micro-services.",
+    icon: React.createElement(CgWorkAlt),
+    date: "Mar 2022 - Feb 2023",
+  },
+  {
+    title: "Data Engineer",
+    location: "Florianópolis, SC",
+    description:
+      "Data engineer that works in multiple projects, using different stacks to deal with big data. Currently in an international project, using technologies like Apache Airflow, Embulk, Meltano, Airbyte, Kubernetes, AWS. Also using infrastructure as a code with Terraform, provisioning the needed inftrastructure to the project. Data engineer that works in multiple projects, using different stacks to deal with big data. Currently in an international project, using technologies like Apache Airflow, Embulk, Meltano, Airbyte, Kubernetes, AWS. Also using infrastructure as a code with Terraform, provisioning the needed inftrastructure to the project. ",
+    icon: React.createElement(CgWorkAlt),
+    date: "Jan 2023 - present",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Supermarket",
+    description:
+      "This was my very first project in college written in Java using object oriented programming.",
+    tags: ["Java", "OOP"],
+    imageUrl: supermarket,
+  },
+  {
+    title: "Deposit",
+    description:
+      "Created a deposit system for a grad in college with Java.",
+    tags: ["Java", "OOP", "Tests"],
+    imageUrl: deposit,
+  },
+  {
+    title: "A*",
+    description:
+      "Implementing A* algorithm with python.",
+    tags: ["Python","A*", "Data Structure"],
+    imageUrl: astar,
+  },
+  {
+    title: "Cryptography",
+    description:
+      "Implementing file encryption with AES and PBKDF2 using python, using Fernet Keys to store password in a security maner.",
+    tags: ["Python","AES", "Fernet Key","PBKDF2","Cryptography"],
+    imageUrl: astar,
+  },
+  {
+    title: "Full Page App",
+    description:
+      "Implementing a VueJs Single Page to deal with political data to give transparency about how public finds are spent.",
+    tags: ["VueJs","SCSS", "HTML","API","Rest"],
+    imageUrl: full_page,
+  },
+  {
+    title: "Undergraduate Thesis",
+    description:
+      "This project aimed to develop a predictive model for student text grades using NLP techniques.",
+    tags: ["JupyterNotebook","Spacy", "PyTorch","BERT","Scikit-learn", "TF-IDF", "POS", "NER", "ML"],
+    imageUrl: tcc,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "MongoDB",
+  "Redux",
+  "GraphQL",
+  "Apollo",
+  "Express",
+  "PostgreSQL",
+  "Python",
+  "Django",
+  "Framer Motion",
+] as const;
